fbb4f120a3f7c7bee30ce9f629793ea1d58fefd0
alviseaspesi <alviseaspesi@gmail.com>
2019-11-27 12:56:18 +0100

    04

--- sketch.js
+++ sketch.js
@@ -3,8 +3,7 @@ var copie = [];
 var amp;
 var playButton;
 var restartButton;
-var tromba;
-var trump;
+
 
 function preload(){
   font = loadFont("./assets/gt-walsheim-bold.ttf");
@@ -20,25 +19,28 @@ function setup() {
   amp = new p5.Amplitude();
   amp.setInput(mySong);
 
-  copie = new titolo();
-  tromba = new Tromba();
-  trump = new Trump();
-
-  playButton = new pB();
-  restartButton = new rB();
 }
 
 function draw() {
   background("black");
 
-  copie.display();
-  trump.display();
-  tromba.display();
+  imageMode(CENTER);
+  volume = amp.getLevel();
+  volume = map(volume, 0, 1, 1, 5);
+  image(logo, windowWidth / 2 * volume, windowHeight / 2, 100 / 3, 71 / 3);
 
-  playButton.display();
-  restartButton.display();
+  imageMode(CENTER);
+  image(trumpp, windowWidth / 2 - 510, windowHeight / 2 - 20, 706 / 2.5, 672 / 2.5);
 
+  imageMode(CENTER);
+  image(trumpet, windowWidth / 2 - 180, windowHeight / 2 + 20, 640 / 1.5 , 210 / 1.5 );
 
+  rectMode(CENTER);
+  noStroke();
+  fill(color("#37123C"));
+  rect(windowWidth / 3, windowHeight - 100, 90, 30);
+
+  rect(2 * (width / 3), windowHeight - 100, 90, 30);
 
   textAlign(CENTER, CENTER);
   textSize(10);
@@ -53,88 +55,41 @@ function draw() {
   text("refresh", 2 * (windowWidth / 3), windowHeight  - 102);
 }
 
-function titolo() {
-  this.x = windowWidth / 2;
-  this.y = windowHeight / 2;
-
-  this.display = function() {
-    imageMode(CENTER);
-    volume = amp.getLevel();
-    volume = map(volume, 0, 1, 1, 5);
-    image(logo, windowWidth / 2 * volume, this.y, 100 / 3, 71 / 3);
-  }
-}
-
-function Trump() {
-  this.x = windowWidth / 2 - 510;
-  this.y = windowHeight / 2 - 20;
-
-  this.display = function() {
-    imageMode(CENTER);
-    image(trumpp, this.x, this.y, 706 / 2.5, 672 / 2.5);
-  }
-}
-
-function Tromba() {
-  this.x = windowWidth / 2 - 180;
-  this.y = windowHeight / 2 + 20;
-
-  this.display = function() {
-    imageMode(CENTER);
-    image(trumpet, this.x, this.y, 640 / 1.5 , 210 / 1.5 );
-  }
-}
 
 
-function pB() {
-  this.xr = windowWidth / 3;
-  this.yr = windowHeight - 100;
-  this.dw = 90;
-  this.dh = 30;
-  this.colorr = color("#37123C");
 
-  this.display = function() {
-    rectMode(CENTER);
-    noStroke();
-    fill(this.colorr);
-    rect(this.xr, this.yr, this.dw, this.dh);
-  }
+// function rB() {
+//   this.xr = 2 * (width / 3);
+//   this.yr = height - 100;
+//   this.dw = 90;
+//   this.dh = 30;
+//   this.colorr = color("#37123C");
+//
+//   this.display = function() {
+//     rectMode(CENTER);
+//     noStroke();
+//     fill(this.colorr);
+//     rect(this.xr, this.yr, this.dw, this.dh);
+//   }
+//
+//   this.restart = function() {
+//     var d = dist(mouseX, mouseY, this.xr, this.yr);
+//     if (d < this.dw && d < this.dh) {
+//       document.location.reload();
+//     }
+//   }
+// }
 
-  this.start = function() {
-    var d = dist(mouseX, mouseY, this.xr, this.yr);
-    if (d < this.dw && d < this.dh) {
-      if (!mySong.isPlaying()) {
-        mySong.play();
-      } else {
-        mySong.pause();
-      }
-    }
-  }
-}
-function rB() {
-  this.xr = 2 * (width / 3);
-  this.yr = height - 100;
-  this.dw = 90;
-  this.dh = 30;
-  this.colorr = color("#37123C");
-
-  this.display = function() {
-    rectMode(CENTER);
-    noStroke();
-    fill(this.colorr);
-    rect(this.xr, this.yr, this.dw, this.dh);
-  }
-
-  this.restart = function() {
-    var d = dist(mouseX, mouseY, this.xr, this.yr);
-    if (d < this.dw && d < this.dh) {
-      document.location.reload();
-    }
-  }
-}
 function mousePressed() {
-  playButton.start();
-  restartButton.restart();
+  if (mouseX > windowWidth / 3 - 45 && mouseX < windowWidth / 3 + 45 && mouseY > windowHeight - 115 && mouseY < windowHeight - 85){
+    if (!mySong.isPlaying()) {
+      mySong.play();
+    } else {
+      mySong.pause();
+    }
+  } else if(mouseX > 2 * (width / 3) - 45 && mouseX < 2 * (width / 3) + 45 && mouseY > windowHeight - 115 && mouseY < windowHeight - 85){
+    document.location.reload();
+  }
 }
 
 function windowResized() {
