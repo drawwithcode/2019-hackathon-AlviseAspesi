79d466ac934d0f69260146ca0d2ea98bc7b6e576
alviseaspesi <alviseaspesi@gmail.com>
2019-11-27 12:39:56 +0100

    02

--- sketch.js
+++ sketch.js
@@ -3,11 +3,15 @@ var copie = [];
 var amp;
 var playButton;
 var restartButton;
+var tromba;
+var trump;
 
 function preload(){
   font = loadFont("./assets/gt-walsheim-bold.ttf");
   mySong = loadSound("./assets/TG1_bumper.mp3");
   logo = loadImage("./assets/tg1.png");
+  trumpet = loadImage("./assets/tromba.png");
+  trumpp = loadImage("./assets/trump.png");
 }
 
 function setup() {
@@ -16,26 +20,25 @@ function setup() {
   amp = new p5.Amplitude();
   amp.setInput(mySong);
 
-  for (var i = 0; i < 50; i++) {
-    copie[i] = new titolo();
-  };
+  copie = new titolo();
+  tromba = new Tromba();
+  trump = new Trump();
 
   playButton = new pB();
   restartButton = new rB();
 }
 
 function draw() {
-  var sampleIsLooping = false;
-
   background("black");
 
-  for (var i = 0; i < copie.length; i++) {
-    copie[i].move();
-    copie[i].display();
-  }
+  copie.display();
+  trump.display();
+  tromba.display();
 
   playButton.display();
   restartButton.display();
+
+
 
   textAlign(CENTER, CENTER);
   textSize(10);
@@ -47,28 +50,45 @@ function draw() {
   textSize(10);
   fill(color("white"));
   textFont(font);
-  text("restart", 2 * (windowWidth / 3), windowHeight  - 102);
+  text("refresh", 2 * (windowWidth / 3), windowHeight  - 102);
 }
 
 function titolo() {
-  this.x = random(0, width);
-  this.y = random(0, height);
+  this.x = windowWidth / 2;
+  this.y = windowHeight / 2;
 
   this.display = function() {
+    imageMode(CENTER);
     volume = amp.getLevel();
-    volume = map(volume, 0, 1, 1, 80);
-    image(logo, this.x, this.y, 3 * volume, 3 * volume);
-  }
-
-  this.move = function() {
-    this.x = this.x + random(-1, 1);
-    this.y = this.y + random(-1, 1);
+    volume = map(volume, 0, 1, 1, 5);
+    image(logo, windowWidth / 2 * volume, this.y, 100 / 3, 71 / 3);
   }
 }
 
+function Trump() {
+  this.x = windowWidth / 2 - 510;
+  this.y = windowHeight / 2 - 20;
+
+  this.display = function() {
+    imageMode(CENTER);
+    image(trumpp, this.x, this.y, 706 / 2.5, 672 / 2.5);
+  }
+}
+
+function Tromba() {
+  this.x = windowWidth / 2 - 180;
+  this.y = windowHeight / 2 + 20;
+
+  this.display = function() {
+    imageMode(CENTER);
+    image(trumpet, this.x, this.y, 640 / 1.5 , 210 / 1.5 );
+  }
+}
+
+
 function pB() {
-  this.xr = width / 3;
-  this.yr = height - 100;
+  this.xr = windowWidth / 3;
+  this.yr = windowHeight - 100;
   this.dw = 90;
   this.dh = 30;
   this.colorr = color("#37123C");
@@ -116,6 +136,7 @@ function mousePressed() {
   playButton.start();
   restartButton.restart();
 }
+
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
 }
